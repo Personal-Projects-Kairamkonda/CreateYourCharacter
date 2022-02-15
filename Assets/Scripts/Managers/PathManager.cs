cf8e1c76cfa08175371c3047f77a342a98987333
Sravan Kairamkonda <35738653+SravanGameDev@users.noreply.github.com>
2022-02-15 15:19:40 +0000

    Added path but bugs when it freeze both positions

--- Assets/Scripts/Managers/PathManager.cs
+++ Assets/Scripts/Managers/PathManager.cs
@@ -7,7 +7,15 @@ public class PathManager : MonoBehaviour
     [SerializeField]
     RandomPosition randomPosition;
 
+
+    /// <summary>
+    /// x-axis freeze
+    /// </summary>
     private bool freezeX;
+
+    /// <summary>
+    /// z-axis freeze
+    /// </summary>
     private bool freezeZ;
 
     void Start()
@@ -15,42 +23,74 @@ public class PathManager : MonoBehaviour
         StartCoroutine(CreateShape());
     }
 
+    /// <summary>
+    /// Instantaite the shape and its position
+    /// </summary>
+    /// <returns></returns>
     IEnumerator CreateShape()
     {
         while (true)
         {
             GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-            //if(randomPosition.newX)
-
             temp.transform.position = new Vector3(randomPosition.newX,
                                                   randomPosition.newY,
                                                   randomPosition.newZ);
 
             temp.transform.eulerAngles = new Vector3(0, 0, 0);
+
             temp.transform.SetParent(this.transform);
+
+            temp.gameObject.name = temp.transform.position.ToString();
 
             freezeX = freeposition(freezeX);
             freezeZ = !freezeX;
 
             if (freezeX)
             {
-                randomPosition.newZ++;
-                randomPosition.newZ += 0.2f;
+                bool direction = ChangeDirection();
+
+                Debug.Log("Direction: " + direction);
+
+                if (direction)
+                {
+                    randomPosition.newZ++;
+                    randomPosition.newZ += 0.2f;
+                }
+                else
+                {
+                    randomPosition.newZ--;
+                    randomPosition.newZ -= 0.2f;
+                }
             }
 
             if (freezeZ)
             {
-                randomPosition.newX++;
-                randomPosition.newX += 0.2f;
+                bool direction = ChangeDirection();
+
+                Debug.Log("Direction: " + direction);
+
+                if (direction)
+                {
+                    randomPosition.newX++;
+                    randomPosition.newX += 0.2f;
+                }
+                else
+                {
+                    randomPosition.newX--;
+                    randomPosition.newX -= 0.2f;
+                }
             }
 
-            
-
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1.2f);
         }
     }
 
+    /// <summary>
+    /// Random freeze the position
+    /// </summary>
+    /// <param name="x">bool value of axis</param>
+    /// <returns>random boolean </returns>
     bool freeposition(bool x)
     {
         int value = Random.Range(0, 2);
@@ -65,8 +105,30 @@ public class PathManager : MonoBehaviour
         }
 
     }
+
+    /// <summary>
+    /// Random boolean generator
+    /// </summary>
+    /// <returns> a boolean value</returns>
+    bool ChangeDirection()
+    {
+        int value = Random.Range(0, 2);
+
+        if (value == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
 
+/// <summary>
+/// Store random position of each axis
+/// </summary>
 [System.Serializable]
 public class RandomPosition
 {
