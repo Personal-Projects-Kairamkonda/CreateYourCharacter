a591867d459a7b358fe9ea55404ef6dc0d0cf346
Sravan Kairamkonda <35738653+SravanGameDev@users.noreply.github.com>
2022-02-27 20:00:42 +0000

    Positioning of the character

--- Assets/Scripts/Managers/OptimizeCharacterManager.cs
+++ Assets/Scripts/Managers/OptimizeCharacterManager.cs
@@ -5,11 +5,25 @@ using UnityEngine.UI;
 
 public class OptimizeCharacterManager : MonoBehaviour
 {
-   public Transform characterParent;
+    /// <summary>
+    /// Character parent
+    /// </summary>
+    public Transform characterParent;
 
+    /// <summary>
+    /// Character button
+    /// </summary>
     public Button createCharacterButton;
 
+    /// <summary>
+    /// Back button
+    /// </summary>
     public Button backButton;
+
+    /// <summary>
+    /// Layout parent
+    /// </summary>
+    public GameObject layout;
 
     private void Start()
     {
@@ -17,16 +31,23 @@ public class OptimizeCharacterManager : MonoBehaviour
         backButton.onClick.AddListener(Back);
     }
 
+    /// <summary>
+    /// This function creates character 
     public void CreateCharacter()
     {
         characterParent.localScale = Vector3.one * 0.8f;
         characterParent.localPosition = Vector3.zero;
         characterParent.localPosition = new Vector3(-650f, 0, 0);
+        layout.SetActive(true);
     }
 
+    /// <summary>
+    /// This function gives ability to edit the character
+    /// </summary>
     public void Back()
     {
         characterParent.localScale = Vector3.one*1.5f;
         characterParent.localPosition = new Vector3(-10, -102, 0);
+        layout.SetActive(false);
     }
 }
