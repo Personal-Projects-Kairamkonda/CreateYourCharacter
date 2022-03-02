a68044d9723d444ce8bdd3bda6b6256b455102dd
Sravan Kairamkonda <35738653+SravanGameDev@users.noreply.github.com>
2022-03-02 00:15:56 +0000

    Build is working with itch.io page

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,16 +6,26 @@ public class GameManager : MonoBehaviour
 {
     public GameObject meshGenerator;
 
-    private void Start()
-    {
-        GameObject temp = Instantiate(meshGenerator, transform.position, Quaternion.identity, this.transform);
-    }
-
     public void Regenerate()
     {
         GameObject previousObj = this.transform.GetChild(0).gameObject;
         Destroy(previousObj);
 
         GameObject temp = Instantiate(meshGenerator, transform.position, Quaternion.identity, this.transform);
+    }
+
+
+    public void Isometric()
+    {
+        Camera.main.transform.position = new Vector3(-50, 40, -50);
+        Camera.main.transform.localEulerAngles = new Vector3(30, 45, 0);
+        Camera.main.orthographicSize = 10f;
+    }
+
+    public void TopView()
+    {
+        Camera.main.transform.position = new Vector3(0, 10, 0);
+        Camera.main.transform.localEulerAngles = new Vector3(90, 0, 0);
+        Camera.main.orthographicSize = 25f;
     }
 }
